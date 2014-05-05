ce9f174438dfee588909c0574e79aed2649b8e9d
kenta <kentasato718@gmail.com>
2014-05-05 16:33:28 +0900

    extending registration form
    tutorial 11
    enable email input for user registration

--- django_test/views.py
+++ django_test/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.contrib import auth
 from django.core.context_processors import csrf
-from django.contrib.auth.forms import UserCreationForm
-
+from forms import MyRegistrationForm
 
 def login(request):
 	c = {}
@@ -34,7 +33,7 @@ def logout(request):
 	
 def register_user(request):
 	if request.method == 'POST':
-		form = UserCreationForm(request.POST)
+		form = MyRegistrationForm(request.POST)
 		if form.is_valid():
 			form.save()
 			return HttpResponseRedirect('/accounts/register_success')
@@ -42,7 +41,7 @@ def register_user(request):
 	args = {}
 	args.update(csrf(request))
 	
-	args['form'] = UserCreationForm()
+	args['form'] = MyRegistrationForm()
 	print args
 	return render_to_response('register.html', args)	
 	
